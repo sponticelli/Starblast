e8d76f810c7f1fafee39c7d6570e5219eb5fec3d
Sandro Ponticelli <s.ponticelli@whatwapp.com>
2024-09-08 01:13:08 +0200

    feat: add pool for the weapon

--- Assets/Scripts/Actors/Weapons/Bullet.cs
+++ Assets/Scripts/Actors/Weapons/Bullet.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace Starblast.Actors.Weapons
 {
@@ -14,6 +15,9 @@ namespace Starblast.Actors.Weapons
         private float _currentLifeTime;
         private Vector2 _direction;
         private Vector2 _shipVelocity;
+        private IObjectPool<Bullet> _bulletPool = null;
+        
+        private bool _isReleased = false;
         
         public void Shoot(Vector3 direction, Vector2 shipVelocity)
         {
@@ -21,23 +25,29 @@ namespace Starblast.Actors.Weapons
             _shipVelocity = shipVelocity;
             _direction = direction.normalized;
             _currentLifeTime = _lifeTime;
-            
+            _isReleased = false;
         }
 
         
         private void Update()
         {
             _currentLifeTime -= Time.deltaTime;
-            if (_currentLifeTime <= 0)
+            if (_currentLifeTime <= 0 && !_isReleased)
             {
-                Destroy(gameObject);
+                _bulletPool.Release(this);
+                _isReleased = true;
             }
+
         }
         
         private void FixedUpdate()
         {
             _rigidbody.velocity = (_direction * _speed) + _shipVelocity;
         }
-        
+
+        public void Initialize(IObjectPool<Bullet> bulletPool)
+        {
+            _bulletPool = bulletPool;
+        }
     }
 }
--- Assets/Scripts/Actors/Weapons/Weapon.cs
+++ Assets/Scripts/Actors/Weapons/Weapon.cs
@@ -1,95 +1,132 @@
 using System.Collections;
+using System.Collections.Generic;
 using Starblast.Data;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Pool;
 
 namespace Starblast.Actors.Weapons
 {
+    /// <summary>
+    /// Represents a weapon that can be attached to a spaceship.
+    /// </summary>
     public class Weapon : MonoBehaviour
     {
-        [SerializeField] private Transform _muzzle;
-        [SerializeField] private int _ammo = 10;
-        
-        [field: SerializeField]
-        public UnityEvent OnShoot { get; set; }
-        
-        [field: SerializeField]
-        public UnityEvent OnShootNoAmmo { get; set; }
-        
+        [Header("References")]
+        [SerializeField] private Transform _muzzle = null!;
+
+        [Header("Events")]
+        [SerializeField] private UnityEvent OnShoot = null!;
+        [SerializeField] private UnityEvent OnShootNoAmmo = null!;
+
+        private IObjectPool<Bullet> _bulletPool = null!;
+        private IVelocityProvider _velocityProvider = null!;
+        private IWeaponDataProvider _weaponDataProvider = null!;
+
+        private int _ammo;
+        private bool _isShooting;
+        private bool _isReloading;
+
         public int Ammo
         {
             get => _ammo;
-            set
-            {
-                _ammo = Mathf.Clamp(value, 0, _weaponDataProvider.WeaponData.AmmoCapacity);
-            }
+            set => _ammo = Mathf.Clamp(value, 0, _weaponDataProvider?.WeaponData.AmmoCapacity ?? 0);
         }
-        
-        public bool AmmoEmpty => _ammo <= 0;
-        public bool AmmoFull => _ammo >= _weaponDataProvider.WeaponData.AmmoCapacity;
-        
-        private bool _isShooting;
-        private bool _reloadCoroutine = false;
-        
-        private IVelocityProvider _velocityProvider;
-        private IWeaponDataProvider _weaponDataProvider;
+
+        public bool AmmoEmpty => Ammo <= 0;
+        public bool AmmoFull => Ammo >= _weaponDataProvider?.WeaponData.AmmoCapacity;
 
         private void Start()
         {
+            if (_weaponDataProvider == null || _velocityProvider == null)
+            {
+                Debug.LogError("Weapon not properly initialized. Please call Initialize before use.");
+                enabled = false;
+                return;
+            }
+
             Ammo = _weaponDataProvider.WeaponData.AmmoCapacity;
+            InitializeBulletPool();
         }
-        
-        private void Update()
+
+        private void Update() => UseWeapon();
+
+        /// <summary>
+        /// Initializes the weapon with necessary dependencies.
+        /// </summary>
+        public void Initialize(IWeaponDataProvider weaponDataProvider, IVelocityProvider velocityProvider)
         {
-            UseWeapon();
+            _weaponDataProvider = weaponDataProvider ?? throw new System.ArgumentNullException(nameof(weaponDataProvider));
+            _velocityProvider = velocityProvider ?? throw new System.ArgumentNullException(nameof(velocityProvider));
+
+            if (_muzzle != null)
+            {
+                _muzzle.localPosition = _weaponDataProvider.WeaponData.MuzzleOffset;
+            }
+            else
+            {
+                Debug.LogError("Muzzle transform not assigned to the Weapon.");
+            }
         }
-        public void TryShooting()
+
+        private void InitializeBulletPool()
         {
-            _isShooting = true;
+            _bulletPool = new ObjectPool<Bullet>(
+                createFunc: CreateBullet,
+                actionOnGet: bullet => bullet.gameObject.SetActive(true),
+                actionOnRelease: bullet => bullet.gameObject.SetActive(false),
+                actionOnDestroy: bullet => Destroy(bullet.gameObject),
+                collectionCheck: false,
+                defaultCapacity: 20,
+                maxSize: 100
+            );
         }
-        
-        public void StopShooting()
+
+        private Bullet CreateBullet()
         {
-            _isShooting = false;
+            var bullet = Instantiate(_weaponDataProvider.WeaponData.BulletPrefab, _muzzle.position, _muzzle.rotation);
+            bullet.Initialize(_bulletPool);
+            return bullet;
         }
-        
-        public void Reload(int ammo)
-        {
-            Ammo += ammo;
-        }
-        
+
+        public void TryShooting() => _isShooting = true;
+
+        public void StopShooting() => _isShooting = false;
+
+        public void Reload(int ammo) => Ammo += ammo;
+
         private void UseWeapon()
         {
-            if (_isShooting && !_reloadCoroutine)
+            if (!_isShooting || _isReloading || AmmoEmpty) return;
+
+            Ammo--;
+            OnShoot?.Invoke();
+
+            for (int i = 0; i < _weaponDataProvider.WeaponData.GetBulletCountToSpawn; i++)
             {
-                if (!AmmoEmpty)
-                {
-                    Ammo--;
-                    OnShoot?.Invoke();
-                    for(var i = 0; i < _weaponDataProvider.WeaponData.GetBulletCountToSpawn; i++)
-                    {
-                        ShootBullet();
-                    }
-                }
-                else
-                {
-                    _isShooting = false;
-                    OnShootNoAmmo?.Invoke();
-                    return;
-                }
+                ShootBullet();
+            }
+
+            if (AmmoEmpty)
+            {
+                _isShooting = false;
+                OnShootNoAmmo?.Invoke();
+            }
+            else
+            {
                 FinishShooting();
             }
         }
 
         private void ShootBullet()
         {
-            var bullet = Instantiate(_weaponDataProvider.WeaponData.BulletPrefab, _muzzle.position, _muzzle.rotation);
-            var direction = _muzzle.up;
-            // Add spread angle to the direction
-            direction = Quaternion.Euler(0, 0, Random.Range(-_weaponDataProvider.WeaponData.SpreadAngle, _weaponDataProvider.WeaponData.SpreadAngle)) * direction;
+            var bullet = _bulletPool.Get();
+            bullet.transform.SetPositionAndRotation(_muzzle.position, _muzzle.rotation);
+
+            var direction = Quaternion.Euler(0, 0, Random.Range(-_weaponDataProvider.WeaponData.SpreadAngle, _weaponDataProvider.WeaponData.SpreadAngle)) * _muzzle.up;
             bullet.Shoot(direction, _velocityProvider.GetVelocity());
         }
-        
+
         private void FinishShooting()
         {
             StartCoroutine(DelayNextShootCoroutine());
@@ -98,28 +135,19 @@ namespace Starblast.Actors.Weapons
                 _isShooting = false;
             }
         }
-        
+
         private IEnumerator DelayNextShootCoroutine()
         {
-            _reloadCoroutine = true;
+            _isReloading = true;
             yield return new WaitForSeconds(_weaponDataProvider.WeaponData.WeaponDelay);
-            _reloadCoroutine = false;
+            _isReloading = false;
         }
-        
+
         private void OnDrawGizmos()
         {
             if (_muzzle == null) return;
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(_muzzle.transform.position, 0.1f);
+            Gizmos.DrawWireSphere(_muzzle.position, 0.1f);
         }
-
-        public void Initialize(IWeaponDataProvider weaponDataProvider, IVelocityProvider velocityProvider)
-        {
-            _velocityProvider = velocityProvider;
-            _weaponDataProvider = weaponDataProvider;
-            
-            _muzzle.localPosition = _weaponDataProvider.WeaponData.MuzzleOffset;
-        }
-
     }
 }
